b918884ee7caad34f36ad8d43a27dd576f962c19
lr-can <103670765+lr-can@users.noreply.github.com>
2024-04-05 16:11:02 +0200

    not logged page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,70 @@
+<template>
+  <div v-if="notAuthenticated">
+    <div id="messageErreur">
+      <span id="redAlert">
+        Non connecté
+      </span>
+    </div>
+    <div id="message">
+      Bonjour,
+    </div>
+    <div>
+      Cette application est reservée au personnel dûment authentifié.
+    </div>
+    <div id="loginBtn" @click="login">
+      Se connecter
+    </div>
+  
+  </div>
+
+  <div v-if="!notAuthenticated">
+    Bienvenue, {{ nomUtilisateur }} !
+  </div>
+</template>
 <script setup>
+import { ref } from "vue";
+
+const notAuthenticated = ref(true);
+
+const login = () => {
+  console.log('button clicked')
+  notAuthenticated.value = false;
+}
+
 
 </script>
 
-<template>
-  Hello !
-</template>
+<style>
+#messageErreur{
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  font-size: 12px;
+  border-radius: 5px;
+  background-color: #fff4f4;
+  color: #f60700;
+  width: 30vw;
+  text-align: center;
+  height: 1.5rem;
+  padding: 0.2rem 0;
+}
+#message{
+  font-size: 16px;
+  margin-bottom: 1rem;
+  margin-top: 1.5rem;
+}
+#loginBtn{
+  margin-top: 2rem;
+  border-radius: 20px;
+  background-image: linear-gradient(to right bottom, #0078f3 5%, #18753c 100%);
+        background-size: 140% 140%;
+			animation: gradient 2s ease infinite;
+  height: 2.2rem;
+  text-align: center;
+  padding: 0.4rem 0;
+  color: white;
+  max-width: 60vw;
+  min-width: 15vw;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
