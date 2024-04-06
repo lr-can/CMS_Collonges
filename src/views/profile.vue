551d333018781dc1ff5a056b123e39094c67b475
lr-can <103670765+lr-can@users.noreply.github.com>
2024-04-06 22:34:29 +0200

    added some part in the profile

--- src/views/profile.vue
+++ src/views/profile.vue
@@ -4,7 +4,7 @@
     </div>
     <div class="information-layout">
       <div class="information-grade">
-        <img :src="image_grade(grade)" width="90vw" height="auto">
+        <img :src="image_grade(grade)" width="85vw" height="auto">
       </div>
       <div class="information-texte">
         <span id="grade">
@@ -16,12 +16,26 @@
         </span>
         <br>
         <span id="matricule">{{ matricule }}</span>
-        
+      </div>
+    </div>
+    <div class="title">
+      Rôle
+    </div>
+    <div class="information-layout">
+      <div class="information-grade">
+        <img :src="image_role(role)" width="85vw" height="auto">
+      </div>
+      <div class="information-texte">
+        <span>
+          {{ role }}
+        </span>
       </div>
     </div>
     <div>
-    <div @click="logout" id="logout">Se déconnecter</div>
-  </div>
+      <div @click="logout" id="logout">
+        Se déconnecter
+      </div>
+    </div>
 </template> 
 
 <script setup>
@@ -35,9 +49,14 @@ let utilisateur = auth0.user.value;
 const matricule = ref(utilisateur.profile[0]);
 const grade = ref(utilisateur.profile[1]);
 const nom = ref(utilisateur.name);
+const role = ref(utilisateur.profile[2]);
 
 const image_grade = (current_grade) => {
   return `https://github.com/lr-can/CMS_Collonges/blob/main/src/assets/grades/${current_grade}.png?raw=true`
+};
+
+const image_role = (current_role) => {
+  return `https://github.com/lr-can/CMS_Collonges/blob/main/src/assets/roles/${current_role}.png?raw=true`
 };
 
 const logout = () => {
