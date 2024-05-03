95c06522abb12b54181a0d132c10abb396b693ff
lr-can <103670765+lr-can@users.noreply.github.com>
2024-05-03 14:43:41 +0200

    tried a new way

--- vite.config.js
+++ vite.config.js
@@ -2,26 +2,40 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'CMS Collonges',
+        short_name: 'CMS Collonges',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/assets/logo.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/assets/logo.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  pwa: {
-    name: 'CMS Collonges',
-    themeColor: '#0078f3',
-    msTileColor: '#000000',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      swSrc: 'dev/sw.js'
-    }
-  }
+  
 })
