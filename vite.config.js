a8b269661b84494a1f611e80fa3df67e9e94d2a7
lr-can <103670765+lr-can@users.noreply.github.com>
2024-05-03 14:48:01 +0200

    ezfhkliezf

--- vite.config.js
+++ vite.config.js
@@ -19,12 +19,12 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: '/assets/logo.png',
+            src: '@/assets/logo.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/assets/logo.png',
+            src: '@/assets/logo.png',
             sizes: '512x512',
             type: 'image/png',
           },
