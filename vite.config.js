d90a99625272d807d4fec5d56b0b9e85d421a31b
lr-can <103670765+lr-can@users.noreply.github.com>
2024-08-27 22:26:36 +0200

    autoUpdatenow

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
-      registerType: 'prompt',
+      registerType: 'autoUpdate',
       devOptions: {
         enabled: true,
       },
