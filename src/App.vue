9a29322f2fd7431c50913008f05d968d316c3532
lr-can <103670765+lr-can@users.noreply.github.com>
2024-08-31 10:07:33 +0200

    tried to fix the issue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <script setup>
 console.log(`%cFait avec amour 🫶, sueur 💪💦 et larmes 🥹 par le Caporal Lorcan BRENDERS pour un stage de projet universitaire.`, "background-color: #f4f6ff; color: #0078f3; text-align:justify; padding: 1rem; border-radius: 1rem;font-size:20px;font-weight:bold;");
-
 import { RouterLink, RouterView } from 'vue-router';
 import { useAuth0 } from '@auth0/auth0-vue';
-import { ref, watch } from 'vue';
+import { ref, watch, onMounted } from 'vue';
 import { useSqlStore } from "@/stores/database.js";
 
 import Bing from '@/assets/sounds/Bing.mp3';
@@ -43,19 +42,15 @@ async function getAuthentification() {
   await new Promise(r => setTimeout(r, 1000));
 }
 
-for (let i = 0; i < 20; i++) {
+onMounted(async () => {
   console.log('checking authentification');
-  if (isAuthenticated.value === null) {
-    getAuthentification();
-  } else {
-    getAuthentification();
-    isAuthenticated.value = auth0.isAuthenticated;
-  }
-}
+  await getAuthentification();
+  isAuthenticated.value = auth0.isAuthenticated;
 
-setTimeout(() => {
-  appLoading.value = false;
-}, 2500);
+  setTimeout(() => {
+    appLoading.value = false;
+  }, 2500);
+});
 
 localStorage.setItem('currentProfile', '');
 
