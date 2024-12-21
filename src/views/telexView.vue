8e8caafd242cf895f7e073d5d690dd248df61766
lr-can <103670765+lr-can@users.noreply.github.com>
2024-12-21 15:45:21 +0100

    added icons

---
+++ src/views/telexView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="subtitle">
+        Création d'un ordre de départ pour manoeuvre
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+</script>
+
+<style scoped>
+/* Your component styles go here */
+</style>
