7e779055ce55e0be319d41a19989f4aabac2d590
lr-can <103670765+lr-can@users.noreply.github.com>
2025-04-08 10:29:51 +0200

    starting retour inter

--- src/views/RIGrandPublic.vue
+++ src/views/RIGrandPublic.vue
@@ -3,7 +3,7 @@
   <div v-if="!step1 && !validationStep">
       Bienvenue sur l'espace de déclaration des retours d'intervention SSUAP.
   </div>
-  <div id="asupForm">
+  <div id="asupForm" v-if="!validationStep">
     <div>
         <div v-if="errorMessage" class="errorMessage">
             <span>{{ errorMessage }}</span>
@@ -47,9 +47,9 @@
                 Intervention
             </div>
             <p>Sélectionnez l'intervention concernée par la déclaration</p>
-            <div class="input">
-                <div id="inputTextDoc">
-                    <Dropdown v-model="selectedInter" @change="step4 = true; autoScrolltoBottom()" :options="lastNotifsArray" optionLabel="label" :filter="true" filterBy="label" :showClear="true" :appendTo="'self'" :class="{ 'p-invalid': responseError }"/>
+            <div class="input grow">
+                <div id="inputTextDoc" style="width: 90%;">
+                    <Dropdown v-model="selectedInter" @change="step4 = true; autoScrolltoBottom()" :options="lastNotifsArray" optionLabel="label" :filter="true" filterBy="label" :showClear="true" :appendTo="'self'" :class="{ 'p-invalid': responseError }" style="width: 90%" placeholder="Sélectionnez une des interventions" />
                 </div>
             </div>
             <div id="interInfo" v-if="selectedInter">
@@ -74,7 +74,7 @@
                 <p>Sélectionnez le véhicule utilisé durant l'intervention</p>
                 <div class="input">
                     <div id="inputTextDoc">
-                        <Listbox v-model="selectedVehicule" @change="step5 = true;" :options="vehicules" filterBy="label" :showClear="true" :appendTo="'self'" :class="{ 'p-invalid': responseError }"/>
+                        <Listbox v-model="selectedVehicule" @change="step5 = true; autoScrolltoBottom()" :options="vehicules" filterBy="label" :showClear="true" :appendTo="'self'" :class="{ 'p-invalid': responseError }"/>
                     </div>
                 </div>
             </div>
@@ -83,11 +83,11 @@
                     Matériel utilisé
                 </div>
                 <div class="input flex sized1">
-                    <InputSwitch v-model="materielUtilise" :trueValue="true" :falseValue="false" id="nivASUP" style="width: 40px;"/>
+                    <InputSwitch v-model="materielUtilise" :trueValue="true" :falseValue="false" id="nivASUP" :disabled="!showButton2" style="width: 40px;"/>
                     <p style="font-size: 0.8rem; font-style: italic;">Du matériel a été utilisé durant l'intervention</p>
                 </div>
                 <div v-if="materielUtilise" class="input flex sized1">
-                    <InputSwitch v-model="onlySimpleMateriel" :trueValue="true" :falseValue="false" id="nivASUP" style="width: 40px;"/>
+                    <InputSwitch v-model="onlySimpleMateriel" :trueValue="true" :falseValue="false" :disabled="!showButton2" id="nivASUP" style="width: 40px;"/>
                     <p style="font-size: 0.8rem; font-style: italic;">Matériel pour bilan utilisé uniquement</p>
                 </div>
             </div>
@@ -98,49 +98,87 @@
                 </div>
                 <p>Oxygénothérapie</p>
                 <div class="input flex sized1">
-                    <InputSwitch v-model="usedOxygene" :trueValue="true" :falseValue="false" id="nivASUP" style="width: 40px;"/>
+                    <InputSwitch v-model="usedOxygene" :trueValue="true" :falseValue="false" id="nivASUP" :disabled="!showButton2" style="width: 40px;"/>
                     <p style="font-size: 0.8rem; font-style: italic;">Oxygène utilisé durant l'intervention</p>
                 </div>
                 <div v-if="usedOxygene" class="input flex sized1">
-                    <InputSwitch v-model="bouteilleRemplacement" :trueValue="true" :falseValue="false" id="nivASUP" style="width: 40px;"/>
-                    <p style="font-size: 0.8rem; font-style: italic;">Au moins d'une des bouteilles a été remplacée</p>
+                    <InputSwitch v-model="bouteilleRemplacement" :trueValue="true" :falseValue="false" id="nivASUP" :disabled="!showButton2" style="width: 40px;"/>
+                    <p style="font-size: 0.8rem; font-style: italic;">Au moins une bouteille a été remplacée</p>
                 </div>
                 <div v-if="bouteilleRemplacement" class="errorMessage" style="font-size:0.9rem">
                     Assurez-vous que {{ selected_chef_agres.label_short }} a bien demandé le remplacement de la bouteille au guichet unique avant de valider la déclaration.
                 </div>
                 <div v-if="usedOxygene" class="input">
-                    <Listbox v-model="listOxygenotherapy" :options="Object.keys(materiels_list.Oxygenotherapie)" :showClear="true" :multiple="true" :appendTo="'self'" :class="{ 'p-invalid': responseError }"/>
+                    <Listbox v-model="listOxygenotherapy" :options="Object.keys(materiels_list.Oxygenotherapie)" :showClear="true" :multiple="true" :disabled="!showButton2" :appendTo="'self'" :class="{ 'p-invalid': responseError }"/>
                 </div>
                 <p>Réanimation/aspiration</p>
                 <div class="input">
-                    <Listbox v-model="listReaAspi" :options="Object.keys(materiels_list['Réanimation/aspiration'])" :showClear="true" :multiple="true" :appendTo="'self'" :class="{ 'p-invalid': responseError }"/>
+                    <Listbox v-model="listReaAspi" :options="Object.keys(materiels_list['Réanimation/aspiration'])" :showClear="true" :multiple="true" :disabled="!showButton2" :appendTo="'self'" :class="{ 'p-invalid': responseError }"/>
                 </div>
                 <p>Plaies/Affections cutanées</p>
                 <div class="input">
-                    <Listbox v-model="listPlaies" :options="Object.keys(materiels_list['Plaies/Affections cutanées'])" :showClear="true" :multiple="true" :appendTo="'self'" :class="{ 'p-invalid': responseError }"/>
+                    <Listbox v-model="listPlaies" :options="Object.keys(materiels_list['Plaies/Affections cutanées'])" :showClear="true" :multiple="true" :disabled="!showButton2" :appendTo="'self'" :class="{ 'p-invalid': responseError }"/>
                 </div>
                 <p>Traumatologie/divers</p>
                 <div v-if="listTraumatologie.includes('Ceinture trochantérienne')" class="errorMessage"  style="font-size:0.9rem;">
                     Assurez-vous que {{ selected_chef_agres.label_short }} a bien demandé le remplacement de la ceinture trochantérienne au guichet unique en précisant le numéro d'intervention ({{ selectedInter.numeroInter }}) avant de valider la déclaration.
                 </div>
                 <div class="input">
-                    <Listbox v-model="listTraumatologie" :options="Object.keys(materiels_list['Traumatologie/divers'])" :showClear="true" :multiple="true" :appendTo="'self'" :class="{ 'p-invalid': responseError }"/>
+                    <Listbox v-model="listTraumatologie" :options="Object.keys(materiels_list['Traumatologie/divers'])" :showClear="true" :multiple="true" :disabled="!showButton2" :appendTo="'self'" :class="{ 'p-invalid': responseError }"/>
                 </div>
                 <p>Kits</p>
                 <div class="input">
-                    <Listbox v-model="listKits" :options="Object.keys(materiels_list.Kits)" :multiple="true" :showClear="true" :appendTo="'self'" :class="{ 'p-invalid': responseError }"/>
+                    <Listbox v-model="listKits" :options="Object.keys(materiels_list.Kits)" :multiple="true" :showClear="true" :appendTo="'self'" :disabled="!showButton2" :class="{ 'p-invalid': responseError }"/>
                 </div>
                 <p>Autres matériels</p>
                 <div class="input">
                     <Textarea v-model="materiel_manual" :rows="5" :cols="30" :showClear="true" :appendTo="'self'" :class="{ 'p-invalid': responseError }"/>
                 </div>
             </div>
-                
-
+            <div v-if="step5 && showButton2" class="input">
+                <div @click="step6 = true; autoScrolltoBottom(); showButton2 = false; autoChangeNettoyage()" class="validationBtn" :disabled="!showButton2">
+                    Valider le matériel
+                </div>
+            </div>
         </div>
+
+        <div v-if="step6" class="step">
+          <div class="subsubtitle">
+            Informations complémentaires
+          </div>
+          <p>
+            Type de désinfection réalisée
+          </p>
+          <div class="input">
+            <Listbox v-model="desinfection_type" :options="['Aucune désinfection', 'Désinfection du matériel seulement', 'Désinfection du matériel et de la cellule', 'Désinfection renforcée']" :showClear="true" :appendTo="'self'" :class="{ 'p-invalid': responseError }"/>
+          </div>
+          <p>
+            Commentaires
+          </p>
+          <div class="input">
+            <Textarea v-model="commentaire" :rows="7" :cols="30" :showClear="true" :appendTo="'self'" placeholder="Exemple : Douleur thoracique chez victime F, transport XRousse... ou Annulé au départ" :class="{ 'p-invalid': responseError }"/>
+          </div>
+          <div v-if="checkIfCanBeShocking()" class="errorMessage" style="font-size:0.9rem">
+            <p>
+            L'intervention sélectionnée peut par nature heurter la sensibilité de certains intervenants. <br>
+            En cas de besoin, vous pouvez contacter la cellule psychologique du SDMIS au <a href="tel:0478785555" style="color: red; text-decoration: underline; font-weight: bold;">04 78 78 55 55</a>.<br>
+            </p>
+          </div>
+          <div class="validationBtn" v-if="step6" @click="sendResult" :disabled="validationStep">Valider la déclaration</div>
     </div>
+    </div>
+
     <div id="blankSpaceBottom"></div>
   </div>
+  <div v-if="validationStep" class="validationMsg">
+            <div v-if="loading_" style="display: flex; justify-content: center; align-items: center;"><img src="@/assets/loading.gif" alt="Loading..." height="100px" width="auto"></div>
+            <div v-if="loading_" style="display: flex; justify-content: center; align-items: center;"><p style="font-size: 1.5rem;">{{ functionMessage }}</p></div>
+            <p v-if="!loading_">Déclaration validée avec succès !</p>
+            <div v-if="!loading_">
+              <img src="@/assets/finished.gif" alt="" width="300px" height="auto">
+            </div>
+            <p v-if="!loading_">Vous pouvez à présent fermer la page.</p>
+        </div>
 </template>
 <script setup>
 import { ref } from 'vue'
@@ -149,12 +187,18 @@ const step2 = ref(false)
 const step3 = ref(false)
 const step4 = ref(false)
 const step5 = ref(false)
+const step6 = ref(false)
 const validationStep = ref(false)
 const errorMessage = ref('')
 const showButton = ref(true)
 const buttonLabel = ref('Rechercher')
+const showButton2 = ref(true)
+const commentaire = ref('')
 const selectedInter = ref(null)
 const selectedVehicule = ref(null)
+const loading_ = ref(false)
+const functionMessage = ref('')
+const desinfection_type = ref("Désinfection du matériel et de la cellule")
 import InputText from 'primevue/inputtext';
 import Dropdown from 'primevue/dropdown';
 import MultiSelect from 'primevue/multiselect';
@@ -162,7 +206,7 @@ import Listbox from 'primevue/listbox';
 import InputSwitch from 'primevue/inputswitch';
 import Textarea from 'primevue/textarea';
 
-const materielUtilise = ref(true);
+const materielUtilise = ref(false);
 const onlySimpleMateriel = ref(false);
 const usedOxygene = ref(false);
 const bouteilleRemplacement = ref(false);
@@ -365,6 +409,11 @@ const updateButtonLabel = () => {
   };
 };
 
+const autoChangeNettoyage = () => {
+  if (!materielUtilise.value){
+    desinfection_type.value = "Aucune désinfection";
+  }
+}
 const autoScrolltoBottom = async () => {
   await new Promise(r => setTimeout(r, 500));
   const element = document.getElementById('blankSpaceBottom');
@@ -446,6 +495,73 @@ const updateDataAgent = (result) => {
   agentMail.value = result.email;
 }
 
+const checkIfCanBeShocking = () => {
+  if (selectedInter.value && selectedInter.value.notificationTitre) {
+    const titre = selectedInter.value.notificationTitre.toLowerCase();
+    return pouvantChoquer.some(choquant => titre.includes(choquant));
+  }
+  return false;
+}
+
+const sendResult = async () =>{
+  loading_.value= true;
+  loading.play();
+  validationStep.value = true;
+  functionMessage.value = "Création de l'objet à envoyer au serveur...";
+  let payload = {
+    interNum: selectedInter.value?.numeroInter || '',
+    interCommune: selectedInter.value?.notificationVille || '',
+    interLib: selectedInter.value?.notificationTitre || '',
+    interDate: selectedInter.value?.notificationDate || '',
+    interLon: selectedInter.value?.notificationLon || '',
+    interLat: selectedInter.value?.notificationLat || '',
+    CAGrade: selected_chef_agres.value?.grade || '',
+    CANom: `${selected_chef_agres.value?.prenom || ''} ${selected_chef_agres.value?.nom || ''}`.trim(),
+    CALib: selected_chef_agres.value?.label_short || '',
+    CAMail: selected_chef_agres.value?.email || '',
+    EQGrade: agentGrade.value || '',
+    EQNom: `${prenomAgent.value} ${nomAgent.value}`.trim(),
+    EQLib: matricule.value || '',
+    EQMail: agentMail.value || '',
+    matUtilise: materielUtilise.value ? 'Oui' : 'Non',
+    onlyMatBilan: onlySimpleMateriel.value ? 'Oui' : 'Non',
+    MatBilan: Object.keys(materiels_list.value.bilan).filter(key => materiels_list.value.bilan[key]).join(';'),
+    MatOxy: listOxygenotherapy.value.join(';'),
+    MatRea: listReaAspi.value.join(';'),
+    MatPlaie: listPlaies.value.join(';'),
+    MatTrauma: listTraumatologie.value.join(';'),
+    MatKits: listKits.value.join(';'),
+    vehiculeRI: selectedVehicule.value || '',
+    ReconditionnementRI: desinfection_type.value || '',
+    CommentaireRI: commentaire.value || ''
+  }
+  functionMessage.value = "Creation de la liste du matériel utilisé...";
+  let listMateriel = [...listOxygenotherapy.value,
+    ...listReaAspi.value,
+    ...listPlaies.value,
+    ...listTraumatologie.value,
+    ...listKits.value
+  ]
+  await new Promise(r => setTimeout(r, 300));
+  functionMessage.value = "Ajout au matériel à vérifier par la cellule pharmacie...";
+  payload.matToCheck = listMateriel;
+  await new Promise(r => setTimeout(r, 200));
+  
+  functionMessage.value = "Envoi de la déclaration au serveur...";
+  try {
+    await sqlStore.sendRIResult(payload);
+    functionMessage.value = "Déclaration envoyée avec succès !";
+    await new Promise(r => setTimeout(r, 500));
+    loading_.value= false;
+    validation.play();
+    loading.pause();
+  } catch (error) {
+    console.error('Erreur lors de l\'envoi de la déclaration:', error);
+    functionMessage.value = "Erreur lors de l'envoi de la déclaration.";
+    errorAudio.play();
+  }
+}
+
 
 </script>
 <style scoped>
@@ -478,6 +594,9 @@ const updateDataAgent = (result) => {
   flex-wrap: nowrap; 
   margin-top: 0;
   padding-top: 0;
+}
+.grow{
+  flex-grow: 1;
 }
 .arrow-button{
   background-color: #0078f3;
@@ -525,7 +644,7 @@ const updateDataAgent = (result) => {
 #blankSpaceBottom{
   margin-bottom: 5rem;
   display: block;
-  height: 2rem;
+  min-height: 2rem;
 }
 #orangeAlert{
   padding: 1rem;
@@ -660,4 +779,5 @@ p{
     text-align: left;
 
 }
+
 </style>
